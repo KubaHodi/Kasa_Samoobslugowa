298cb7592fb809ae5d49248abf5903ab181825c1
Kuba Hodi <jakhod2007@gmail.com>
2026-01-27 14:21:00 +0100

    Added search bar, and several; buttons for chosing products in shop

--- cash.h
+++ cash.h
@@ -34,6 +34,13 @@ namespace app {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,17 +55,106 @@ namespace app {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(CashForm::typeid));
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// cash
+			// textBox1
+			// 
+			this->textBox1->AccessibleName = L"";
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 27.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->textBox1->Location = System::Drawing::Point(569, 27);
+			this->textBox1->MaxLength = 15;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(795, 49);
+			this->textBox1->TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(512, 27);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(51, 49);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label1->ForeColor = System::Drawing::SystemColors::Control;
+			this->label1->Location = System::Drawing::Point(1370, 45);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(245, 31);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Szukaj produktów";
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::DarkSlateGray;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 48, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->button1->ForeColor = System::Drawing::SystemColors::Control;
+			this->button1->Location = System::Drawing::Point(12, 119);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(350, 173);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Pieczywo";
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::DarkSlateGray;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 48, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->button2->ForeColor = System::Drawing::SystemColors::Control;
+			this->button2->Location = System::Drawing::Point(1542, 119);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(350, 173);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"Warzywa";
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::DarkSlateGray;
+			this->button3->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 48, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->button3->ForeColor = System::Drawing::SystemColors::Control;
+			this->button3->Location = System::Drawing::Point(793, 119);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(350, 173);
+			this->button3->TabIndex = 5;
+			this->button3->Text = L"Owoce";
+			this->button3->UseVisualStyleBackColor = false;
+			// 
+			// CashForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::SeaGreen;
 			this->ClientSize = System::Drawing::Size(1904, 1041);
-			this->Name = L"cash";
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"CashForm";
 			this->Text = L"cash";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
